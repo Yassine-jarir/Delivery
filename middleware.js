6b04135cd7ca8d9627ae85b9b551805aee5cf664
yassine <yassinejarir7@gmail.com>
2024-03-24 03:07:06 +0000

    fix middleware

--- middleware.js
+++ middleware.js
@@ -1,37 +1,20 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 import { i18n } from "./i18n";
 
 let defaultLocale = i18n.defaultLocale;
 let locales = i18n.locales;
 
-// Get the preferred locale, similar to above or using a library
-function getLocale(request) {
-  const acceptedLanguage =
-    request.headers.get("accept-language") || defaultLocale;
-  const preferredLocale = acceptedLanguage.split(",")[0].trim().toLowerCase();
-
-  // Ensure the preferred locale is one of the supported locales
-  return locales.includes(preferredLocale) ? preferredLocale : defaultLocale;
-}
-
 export function middleware(request) {
-  // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
-  const pathnameIsMissingLocale = locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
+  const localeInPath = pathname.split("/")[1];
 
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return NextResponse.redirect(
-      new URL(`/${locale}/${pathname}`, request.url)
-    );
+  if (!locales.includes(localeInPath)) {
+    const restPath = pathname.substring(3); // Remove the incorrect locale prefix
+    const newUrl = new URL(`/${defaultLocale}${restPath}`, request.url);
+    return NextResponse.redirect(newUrl);
   }
+
+  return null;
 }
 
 export const config = {
